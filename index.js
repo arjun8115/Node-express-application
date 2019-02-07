fc78a219c957e3087c05fbd6c46e3242eb2b4dff
Arjun Jaiswal <arjunjaiswal775@gmail.com>
2019-02-07 20:07:33 +0530

    public folder added

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const path = require('path')
+const hoganMiddleware = require('hogan-middleware')
 
 const app = express()
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','mustache')
+app.engine('mustache',hoganMiddleware.__express)
+app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/',(req,res,next)=>{
-	res.send('Hello!!')
-})
 
+// app.get('/',(req,res,next)=>{
+// 	res.send('Hello!!')
+// })
+
+// app.get('/json',(req,res,next)=>{
+// 	const data = {
+// 		greeting: 'hello!!'
+// 	}
+// 	res.json(data)
+// })
+
+// app.get('/home',(req,res,next)=>{
+// res.render('home',null)
+// })
+
+const indexRouter = require('./routes/index')
+
+app.use('/',indexRouter)
+   
 app.listen(3000)
+console.log('Server running on http:localhost:3000')
